7247559993a5903cd7e5bbd990b1d13617135673
ChefAharoni <ChefAharoni@proton.me>
2023-12-20 13:26:16 -0500

    Fixed generate_ids.py to correctly generate a complete json file with all the necessary data.

--- UUID/generate_ids.py
+++ UUID/generate_ids.py
@@ -2,60 +2,36 @@ import uuid
 import json
 
 
-def generate_uuids_for_users(user_ids):
-    user_uuid_map = {}
-    for user_id in user_ids:
-        discord_id = user_ids[user_id]
-        user_uuid = str(uuid.uuid4())
-        user_uuid_map[discord_id] = user_uuid
-    return user_uuid_map
+def generate_uuids_for_users(discord_ids):
+    return {discord_id: str(uuid.uuid4()) for discord_id in discord_ids}
+
+
+def combine_data(discord_ids, discord_emails):
+    combined_data = {}
+    for discord_id in discord_ids:
+        uuid = user_uuid_map[discord_id]
+        email = discord_emails.get(discord_id)
+        combined_data[discord_id] = {"uuid": uuid, "email": email}
+    return combined_data
 
 
 # Load discord_ids from discord_ids.json
 with open("UUID/discord_ids.json") as file:
-    discord_ids = json.load(file)
+    discord_ids = json.load(file)  # Assuming this is a list of IDs
 
-
-# Convert discord_ids to a dictionary
-discord_ids_dict = {str(i): discord_ids[i] for i in range(len(discord_ids))}
-
-
-uuid_map = generate_uuids_for_users(discord_ids_dict)
-
-
-# Save uuid_map to a JSON file
-def save_uuid_map_to_json(uuid_map):
-    with open("UUID/uuids.json", "w") as file:
-        json.dump(uuid_map, file, indent=4)
-
+# Generate UUIDs for each Discord ID
+user_uuid_map = generate_uuids_for_users(discord_ids)
 
 # Load discord_emails from discord_emails.json
-try:
-    with open("UUID/discord_emails.json") as file:
-        discord_emails = json.load(file)
-except FileNotFoundError:
-    discord_emails = {}
+with open("UUID/discord_emails.json") as file:
+    discord_emails = json.load(file)  # Assuming this is a dict mapping ID to email
 
+# Combine data
+combined_data = combine_data(discord_ids, discord_emails)
 
-# Append UUIDs to discord_emails
-def append_uuids_to_discord_emails(discord_emails, uuid_map):
-    for email in discord_emails:
-        if email in uuid_map:
-            if isinstance(discord_emails[email], str):
-                discord_emails[email] = {"discord_id": discord_emails[email], "uuid": uuid_map[email], "email": email}
-            else:
-                discord_emails[email]["discord_id"] = discord_emails[email].get("discord_id", discord_emails[email])
-                discord_emails[email]["uuid"] = uuid_map[email]
-                discord_emails[email]["email"] = email
+# Save combined data to a JSON file
+with open("UUID/ids_emails_uuids.json", "w") as file:
+    json.dump(combined_data, file, indent=4)
 
-# Save updated discord_emails to a JSON file
-def save_discord_emails_to_json(discord_emails):
-    with open("UUID/discord_emails.json", "w") as file:
-        json.dump(discord_emails, file, indent=4)
-
-# Print the updated discord_emails
-# print(discord_emails)
-
-if __name__ == "__main__":
-    append_uuids_to_discord_emails(discord_emails, uuid_map)
-    save_discord_emails_to_json(discord_emails)
+# Print to verify
+# print(combined_data)
